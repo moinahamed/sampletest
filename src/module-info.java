8075f6df2abd2b40ed42770131c854c05edbaa40
Moin Ahamed <moinahamed121@gmail.com>
2023-04-03 17:41:18 +0530

    nice

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Moin Ahamed
+ *
+ */
+module Assesment_sample {
+}
---
+++ src/testgithub/Github_Test.java
@@ -0,0 +1,10 @@
+package testgithub;
+
+public class Github_Test {
+
+	public static void main(String[] args) {
+		System.out.println("i did well in test");
+
+	}
+
+}
